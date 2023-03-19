319f79b9fd460580204171f001bfb24530652d36
Harini <123894924+HaRiNii004@users.noreply.github.com>
2023-03-19 11:25:49 +0530

    Update machine_downtime_monitoring_system.ino

--- machine_downtime_monitoring_system.ino
+++ machine_downtime_monitoring_system.ino
@@ -1,83 +1,70 @@
+#define BLYNK_TEMPLATE_ID "TMPLI3nv5am6"
+#define BLYNK_TEMPLATE_NAME "Machine downtime monitoring system"
+#define BLYNK_AUTH_TOKEN "ANgnrHXlx9rwZ2LK6AIeLYalFRsPL46p"
+
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 #include <DHT.h>
-#include <Blynk.h>
+#define BLYNK_PRINT Serial
+WidgetLCD lcd(V4);
 
-char auth[] = "YOUR_AUTH_TOKEN";
+char auth[] = "ANgnrHXlx9rwZ2LK6AIeLYalFRsPL46p";
 char ssid[] = "STUDENT";
 char pass[] = "iot@2025";
 
-#define BLYNK_TEMPLATE_ID "      "
-#define BLYNK_TEMPLATE_NAME "Machine downtime monitoring system"
-#define BLYNK_AUTH_TOKEN "      "
-
-#define BLYNK_PRINT Serial
 #define BLYNK_TIMEOUT_MS 5000
 #define DHTPIN D2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+#define vibration
+int soundValue = analogRead(A0);
+#define VibrationDetected
 
-int vibrationPin = D1;  // Connect vibration sensor to D1 pin
-int soundPin = D2; // Connect sound sensor to D1 pin
-int threshold = 600; // Adjust the threshold value based on your sensor
+
+#define v  D1  // Connect vibration sensor to D1 pin
+#define sound_analog A0
+int threshold = 600; // Adwjust the threshold value based on your sensor
 
 BlynkTimer timer;
 bool isMachineRunning = false;
-bool isVibrationDetected = false;
-
+// bool isVibrationDetected = false;
 
 void setup() {
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
-  while (!Blynk.connected()) {
-    delay(1000);
-  }
-  pinMode(vibrationPin, INPUT);
-  timer.setInterval(5000L, checkMachineStatus);
-  pinMode(soundPin, INPUT);
-  
+  pinMode(D1, INPUT);
+  //pinMode(sound_digital, INPUT);
+  pinMode(sound_analog, INPUT);
+
   dht.begin();
-  }
+  timer.setInterval(5000L, notify);
+}
 
 
 void loop() {
+  notify();
   Blynk.run();
   timer.run();
+
+
+
 }
+void notify()
+{
+  float t = dht.readTemperature();
+  Serial.println(t);
+  int vib = digitalRead(D1);
+   Serial.println(D1);
+   
+  if (t <= 40.0 && vib == 1  ) {
+    Serial.println("machine downtime started");
+    Blynk.logEvent("off", "Machine downtime started");
+    lcd.clear();
+    lcd.print(0, 1, "machine is downtime");
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V2, vib);
+    Blynk.virtualWrite(V3, soundValue);
 
-void checkMachineStatus() {
-  //  float temperature = bme.readTemperature();
-  //  float humidity = bme.readHumidity();
-  //  float pressure = bme.readPressure() / 100.0F;
-  float temperature = random(100,200)/10;
-  float humidity = random(300,400)/10;
-  float pressure = random(400,500)/10;;
 
-  int vibrationValue = digitalRead(vibrationPin);
-  isVibrationDetected = (vibrationValue == HIGH);
-
-  if (temperature >= 40.0 || isVibrationDetected) {
-    if (!isMachineRunning) {
-      Serial.println("Machine downtime started.");
-      isMachineRunning = true;
-    }
-  } else {
-    if (isMachineRunning) {
-      Serial.println("Machine downtime ended.");
-      isMachineRunning = false;
-    }
   }
 }
-
-    
-    void checkSoundLevel() {
-  int soundValue = analogRead(soundPin);
-  Serial.println(soundValue);
-  
-  if (soundValue > threshold) {
-     Serial.println("Machine downtime started.");
-  }
-    }
-  //Blynk.virtualWrite(V0, temperature);
-  //Blynk.virtualWrite(V1, humidity);
-  //Blynk.virtualWrite(V2, VibrationisDetected);
